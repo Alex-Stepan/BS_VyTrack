48f515b20246006bd0bc0f59f7c958073343cbf8
BLSoftUser <bluelaser.soft@gmail.com>
2022-06-10 13:05:02 -0400

    Add Login_Credentials class  to utilities Package

--- src/test/java/com/qa2/vytrack/pages/LoginPage.java
+++ src/test/java/com/qa2/vytrack/pages/LoginPage.java
@@ -1,4 +1,46 @@
 package com.qa2.vytrack.pages;
 
+import com.qa2.vytrack.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+
+    //------------------------------------------------------//
+
+    /** 1. Initialization of the page: */
+    public LoginPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    //------------------------------------------------------//
+
+
+    /** 2. WebElements: */
+
+
+    //UsernameBox
+    @FindBy(css = "input[id='prependedInput']")
+    public WebElement UsernameBox;
+
+    //PasswordBox
+    @FindBy(css = "input[id='prependedInput2']")
+    public WebElement PasswordBox;
+
+    //LoginBtn
+    @FindBy(css = "button[id='_submit']")
+    public WebElement LoginBtn;
+
+
+    //------------------------------------------------------//
+
+
+    /** 3. Methods: */
+
+
+
+    //------------------------------------------------------//
+
+
 }
---
+++ src/test/java/com/qa2/vytrack/utilities/Login_Credentials.java
@@ -0,0 +1,46 @@
+package com.qa2.vytrack.utilities;
+
+import java.util.ArrayList;
+
+public class Login_Credentials {
+
+    /** 1. ArrayList with Correct usernames */
+    public static ArrayList<String> getCorrUsernameList() {
+        //1.1.
+        ArrayList<String> UsernameList = new ArrayList<>();
+
+        //1.2. add elements to ArrayList
+        UsernameList.add("user42");
+        UsernameList.add("user43");
+        UsernameList.add("user44");
+
+        UsernameList.add("storemanager77");
+        UsernameList.add("storemanager78");
+        UsernameList.add("storemanager248");
+
+        UsernameList.add("salesmanager143");
+        UsernameList.add("salesmanager144");
+        UsernameList.add("salesmanager145");
+
+        //1.3.
+        System.out.println("Size of the UsernameList is : " + UsernameList.size() + " elements");
+        System.out.println("------------------------------------------------------");
+        return UsernameList;
+    }
+
+
+    /** 2. ArrayList with Correct passwords */
+    public static ArrayList getCorrPasswords () {
+        //1.
+        ArrayList<String> PasswordsList = new ArrayList<>();
+
+        //2.
+        PasswordsList.add("UserUser123");
+
+        //3.
+        return  PasswordsList;
+    }
+
+
+
+}
