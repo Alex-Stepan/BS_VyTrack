6d39afeb27e849116e8c9c200cfb2239c9ffd3a1
BLSoftUser <bluelaser.soft@gmail.com>
2022-06-10 11:14:21 -0400

    Fake class create

--- src/test/java/com/qa2/vytrack/pages/Fakeclass.java
+++ src/test/java/com/qa2/vytrack/pages/Fakeclass.java
@@ -3,5 +3,13 @@ package com.qa2.vytrack.pages;
 public class Fakeclass {
     public static void main(String[] args) {
         System.out.println("Hello everyone");
+        System.out.println("Hello everyone");
+
+        System.out.println("Hello everyone");
+        System.out.println("Hello everyone");
+        System.out.println("Hello everyone");
+        System.out.println("Hello everyone");
+        System.out.println("Hello everyone");
+
     }
 }
