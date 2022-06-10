88154a490b16ce8dbef214ca15960dbf3313252f
BLSoftUser <bluelaser.soft@gmail.com>
2022-06-10 10:57:47 -0400

    User Story 1 setup

---
+++ src/test/java/com/qa2/vytrack/pages/DashboardPage.java
@@ -0,0 +1,4 @@
+package com.qa2.vytrack.pages;
+
+public class DashboardPage {
+}
---
+++ src/test/java/com/qa2/vytrack/step_definitions/US1_TC1_AccessVehicle_underFleetModule.java
@@ -0,0 +1,4 @@
+package com.qa2.vytrack.step_definitions;
+
+public class US1_TC1_AccessVehicle_underFleetModule {
+}
